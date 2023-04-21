9acba857e199ef2de2a4677eb502cc8b41ea6b5b
geekgary-x <18017158729@163.com>
2023-04-21 13:03:15 +0800

    🎉 init: project

--- test/Modules/Function.cpp
+++ test/Modules/Function.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/Polygon_mesh_processing/merge_border_vertices.h>
+typedef CGAL::Exact_predicates_exact_constructions_kernel Kernel;
+typedef Kernel::Point_3 Point_3;
+typedef CGAL::Surface_mesh<Point_3> surface_mesh;
 namespace Module
 {
 	namespace Function
--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,10 @@
 #include <gtest/gtest.h>
 
 #include "index.h"
+#include <CGAL/Polygon_mesh_processing/IO/polygon_mesh_io.h>
+#include <CGAL/Polygon_mesh_processing/polygon_mesh_to_polygon_soup.h>
 TEST(Module, Function)
 {
+
 	EXPECT_TRUE(Module::Function::main(1, NULL));
 }
